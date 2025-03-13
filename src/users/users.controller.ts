7aff1880f885e9b5a69f23f86f72d3560e85fecd
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-13 13:52:18 +0700

    Update Service and Tested

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,39 +1,36 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller('users')
+@Controller('users') // Defines the base route: /users (This makes all routes start with /users)
 export class UsersController {
+
+    constructor(private readonly usersService: UsersService) { }
+    // Injecting UsersService to handle business logic
 
     @Get() // GET /users or GET /users?role=value
     // EX>>  /users?role=ADMIN
-    findAll(@Query('role') role?:'INTERN'|'ADMIN'|'ENGINEER')
-    {
-        return ['GET API Work']
-    }
-
-    @Get('interns') // Get /users/Interns
-    findAllInterns() {
-        return ['GET /users/Interns Work']
+    findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENGINEER') {
+        return this.usersService.findAll(role)
     }
 
     @Get(':id') // GET /users/:id
     findOne(@Param('id') id: string) {
-        return { id }
+        return this.usersService.findOne(+id)
     }
 
-    @Post() // Post /users
-    create(@Body() user: {}) {
-        return user
+    @Post() // POST /users 
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.create(user)
     }
 
-    @Patch(':id') //PATCH /users/:id
-    update(@Param(':id') id:String, @Body() userUpdate: {})
-    {
-        return {id, ...userUpdate}
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN' }) {
+        return this.usersService.update(+id, userUpdate)
     }
 
     @Delete(':id') // DELETE /users/:id
     delete(@Param('id') id: string) {
-        return { id }
+        return this.usersService.delete(+id)
     }
 }
 
