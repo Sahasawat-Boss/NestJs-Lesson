2325b5891e5a9f5d8fcbaba840383d4296c94b14
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-03-14 09:48:00 +0700

    Test API Passed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'NestJS Server is Running, Lesson 4';
+    return 'NestJS Server is Running...';
   }
 }
 
